11c31adfb5c462555a0707c91d8479122616aad2
Nguyen Phuoc <nguyenphuoc@192.168.1.24>
2024-12-23 09:07:16 +0700

    update images

--- components/home/OurGames/index.vue
+++ components/home/OurGames/index.vue
@@ -9,8 +9,16 @@
       </div>
       <div class="games-container">
         <div
-          class="game-card"
+          class="game-card game-card-pc"
           v-for="(imgUrl, index) in imgCardGamesUrls"
+          :key="index"
+        >
+          <img :src="imgUrl" :alt="'E-Space ' + (index + 1)" />
+        </div>
+
+        <div
+          class="game-card game-card-sp"
+          v-for="(imgUrl, index) in imgCardGamesSPUrls"
           :key="index"
         >
           <img :src="imgUrl" :alt="'E-Space ' + (index + 1)" />
@@ -35,6 +43,19 @@ import imgCardGames10 from "~/assets/images/Card-games_10.png";
 import imgCardGames11 from "~/assets/images/Card-games_11.png";
 import imgCardGames12 from "~/assets/images/Card-games_12.png";
 
+import imgCardGamesSP1 from "~/assets/images/Card-games-sp-1.png";
+import imgCardGamesSP2 from "~/assets/images/Card-games-sp-2.png";
+import imgCardGamesSP3 from "~/assets/images/Card-games-sp-3.png";
+import imgCardGamesSP4 from "~/assets/images/Card-games-sp-4.png";
+import imgCardGamesSP5 from "~/assets/images/Card-games-sp-5.png";
+import imgCardGamesSP6 from "~/assets/images/Card-games-sp-6.png";
+import imgCardGamesSP7 from "~/assets/images/Card-games-sp-7.png";
+import imgCardGamesSP8 from "~/assets/images/Card-games-sp-8.png";
+import imgCardGamesSP9 from "~/assets/images/Card-games-sp-9.png";
+import imgCardGamesSP10 from "~/assets/images/Card-games-sp-10.png";
+import imgCardGamesSP11 from "~/assets/images/Card-games-sp-11.png";
+import imgCardGamesSP12 from "~/assets/images/Card-games-sp-12.png";
+
 const { t } = useI18n();
 
 const imgCardGamesUrls = [
@@ -50,6 +71,21 @@ const imgCardGamesUrls = [
   imgCardGames10,
   imgCardGames11,
   imgCardGames12,
+];
+
+const imgCardGamesSPUrls = [
+  imgCardGamesSP1,
+  imgCardGamesSP2,
+  imgCardGamesSP3,
+  imgCardGamesSP4,
+  imgCardGamesSP5,
+  imgCardGamesSP6,
+  imgCardGamesSP7,
+  imgCardGamesSP8,
+  imgCardGamesSP9,
+  imgCardGamesSP10,
+  imgCardGamesSP11,
+  imgCardGamesSP12,
 ];
 </script>
 
