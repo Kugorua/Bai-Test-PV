0b117c2c52e4fa06204151e10b131c6bd8d54dcd
Nguyen Phuoc <nguyenphuoc@Nguyens-MacBook-Air-2.local>
2024-12-27 13:41:40 +0700

    Update the mobile UI

--- components/home/OurGames/index.vue
+++ components/home/OurGames/index.vue
@@ -9,19 +9,18 @@
       </div>
       <div class="games-container">
         <div
-          class="game-card game-card-pc"
-          v-for="(imgUrl, index) in imgCardGamesUrls"
+          class="game-card"
+          v-for="(item, index) in imgCardGamesUrls"
           :key="index"
         >
-          <img :src="imgUrl" :alt="'E-Space ' + (index + 1)" />
-        </div>
-
-        <div
-          class="game-card game-card-sp"
-          v-for="(imgUrl, index) in imgCardGamesSPUrls"
-          :key="index"
-        >
-          <img :src="imgUrl" :alt="'E-Space ' + (index + 1)" />
+          <img :src="item.img" :alt="'E-Space ' + (index + 1)" />
+          <div class="games-description">
+            <p class="game-title">{{ item.title }}</p>
+            <p class="game-detail">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -42,50 +41,57 @@ import imgCardGames9 from "~/assets/images/Card-games_9.png";
 import imgCardGames10 from "~/assets/images/Card-games_10.png";
 import imgCardGames11 from "~/assets/images/Card-games_11.png";
 import imgCardGames12 from "~/assets/images/Card-games_12.png";
-
-import imgCardGamesSP1 from "~/assets/images/Card-games-sp-1.png";
-import imgCardGamesSP2 from "~/assets/images/Card-games-sp-2.png";
-import imgCardGamesSP3 from "~/assets/images/Card-games-sp-3.png";
-import imgCardGamesSP4 from "~/assets/images/Card-games-sp-4.png";
-import imgCardGamesSP5 from "~/assets/images/Card-games-sp-5.png";
-import imgCardGamesSP6 from "~/assets/images/Card-games-sp-6.png";
-import imgCardGamesSP7 from "~/assets/images/Card-games-sp-7.png";
-import imgCardGamesSP8 from "~/assets/images/Card-games-sp-8.png";
-import imgCardGamesSP9 from "~/assets/images/Card-games-sp-9.png";
-import imgCardGamesSP10 from "~/assets/images/Card-games-sp-10.png";
-import imgCardGamesSP11 from "~/assets/images/Card-games-sp-11.png";
-import imgCardGamesSP12 from "~/assets/images/Card-games-sp-12.png";
-
 const { t } = useI18n();
 
 const imgCardGamesUrls = [
-  imgCardGames1,
-  imgCardGames2,
-  imgCardGames3,
-  imgCardGames4,
-  imgCardGames5,
-  imgCardGames6,
-  imgCardGames7,
-  imgCardGames8,
-  imgCardGames9,
-  imgCardGames10,
-  imgCardGames11,
-  imgCardGames12,
-];
-
-const imgCardGamesSPUrls = [
-  imgCardGamesSP1,
-  imgCardGamesSP2,
-  imgCardGamesSP3,
-  imgCardGamesSP4,
-  imgCardGamesSP5,
-  imgCardGamesSP6,
-  imgCardGamesSP7,
-  imgCardGamesSP8,
-  imgCardGamesSP9,
-  imgCardGamesSP10,
-  imgCardGamesSP11,
-  imgCardGamesSP12,
+  {
+    img: imgCardGames1,
+    title: "E-Space",
+  },
+  {
+    img: imgCardGames2,
+    title: "Pirates",
+  },
+  {
+    img: imgCardGames3,
+    title: "Magic tree",
+  },
+  {
+    img: imgCardGames4,
+    title: "Kingland",
+  },
+  {
+    img: imgCardGames5,
+    title: "Witch Party",
+  },
+  {
+    img: imgCardGames6,
+    title: "Aborigines",
+  },
+  {
+    img: imgCardGames7,
+    title: "The Last Game",
+  },
+  {
+    img: imgCardGames8,
+    title: "Rocky",
+  },
+  {
+    img: imgCardGames9,
+    title: "Cinderella",
+  },
+  {
+    img: imgCardGames10,
+    title: "G-Dragon",
+  },
+  {
+    img: imgCardGames11,
+    title: "Blue Fire",
+  },
+  {
+    img: imgCardGames12,
+    title: "Egypt Game",
+  },
 ];
 </script>
 
